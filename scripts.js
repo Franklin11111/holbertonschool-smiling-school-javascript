8686808b0dcc93cae20d286c61d80010c8b9a732
Franklin11111 <abdulovnail6@gmail.com>
2025-08-24 12:17:47 +0400

    Refactored scripts for homepage

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     function queryQuotes() {
-        displayLoading(true);
-        const dataObj = {
-            action: 'query',
-            format: 'json',
-        };        
+      const dataObj = {
+        action: 'query',
+        format: 'json',
+      };        
+      displayLoading(true, '#carouselExampleControls');
         $.ajax({
             url: 'https://smileschool-api.hbtn.info/quotes',
             data: dataObj,
@@ -12,17 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
             dataType: 'json'
         })
         .done((json) => {
-            $(".quote1 img").attr('src', json[0].pic_url)
-            $(".quote1 p").text(json[0].text);
-            $(".quote1 h4").text(json[0].name);
-            $(".quote1 span").text(json[0].title);
-
-            $(".quote2 img").attr('src', json[1].pic_url)
-            $(".quote2 p").text(json[1].text);
-            $(".quote2 h4").text(json[1].name);
-            $(".quote2 span").text(json[1].title);
-            
-            displayLoading(false);
+          const carouselInner = $('#carouselExampleControls .carousel-inner');
+            const quotes = getQuotesHtml(json);
+            carouselInner.append(quotes);
+                       
+            displayLoading(false, '#carouselExampleControls');
         })
         .fail((xhr, status, errorThrown) => {
             console.log(`Error: ${errorThrown}`);
@@ -30,51 +24,54 @@ document.addEventListener("DOMContentLoaded", () => {
             console.dir(xhr);
         });
     }
+
+    // get Quotes HTML    
+    function getQuotesHtml(data) {
+      let html = '';
+      $.each(data, (index, object) => {
+        html += `
+          <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                      <div class="row mx-auto align-items-center quote1">
+                        <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                          <img
+                            src="${object["pic_url"]}"
+                            class="d-block align-self-center"
+                            alt="Carousel Pic 1"
+                          />
+                        </div>
+                        <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                          <div class="quote-text">
+                            <p class="text-white">
+                              ${object.text}
+                            </p>
+                            <h4 class="text-white font-weight-bold">${object.name}</h4>
+                            <span class="text-white">${object.title}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+          `
+      });
+      return html;
+    }
+    // 
+
     queryQuotes();
-    function displayLoading(loading) {
-        const caruselItem = $(".carousel-inner");
+    function displayLoading(loading, container_id) {
+        const caruselItem = $(`${container_id} .carousel-inner`);
         if (loading) {
-            $(".carousel-item").css('visibility', 'hidden');
-          caruselItem.before('<div class="loader"></div>');
+            //$(".carousel-item").css('visibility', 'hidden');
+            caruselItem.removeClass('d-flex');
+          caruselItem.append('<div class="loader"></div>');
         } else {
+          caruselItem.addClass('d-flex');
           $(".loader").remove();
-          $(".carousel-item").css('visibility', 'visible');
+          //$(".carousel-item").css('visibility', 'visible');
         }
       }
 
 const multipleItemCarousel = document.querySelector("#carouselExampleControls2");
 const multipleItemCarousel3 = document.querySelector("#carouselExampleControls3");
-if(window.matchMedia("(min-width: 576px)").matches) {
-  const carousel = new bootstrap.Carousel(multipleItemCarousel, {
-        interval: false
-    });
-  let carouselWidth = $("#carouselExampleControls2 .carousel-inner")[0].scrollWidth;
-  let cardWidth = $("#carouselExampleControls2 .carousel-item").width();
-  let scrollPosition = 0;
-  const nextBtn = document.querySelector('#carouselExampleControls2 .carousel-control-next.arrow-right')
-
-  nextBtn.addEventListener('click', () => {
-        if(scrollPosition < (carouselWidth - (cardWidth * 4))) {
-        scrollPosition = scrollPosition + cardWidth;
-        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
-        } else {
-          scrollPosition = scrollPosition + cardWidth;
-        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
-        }
-    });
-    $('#carouselExampleControls2 .carousel-control-prev.arrow-left').on('click', function() {
-        if(scrollPosition > 0) {
-        scrollPosition = scrollPosition - cardWidth;
-        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
-        } else {
-          scrollPosition = scrollPosition - cardWidth;
-        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
-        }
-    });
-
-}else if (window.matchMedia("(max-width: 576px)").matches){
-   // $(multipleItemCarousel).addClass('slide');
-}
 
 if(window.matchMedia("(min-width: 576px)").matches) {  
     // -----------------------
@@ -111,7 +108,7 @@ if(window.matchMedia("(min-width: 576px)").matches) {
 
 // Query tutorials
 function queryTutorials() {
-  displayLoading(true);
+  displayLoading(true, "#carouselExampleControls2");
   const dataObj = {
       action: 'query',
       format: 'json',
@@ -123,34 +120,10 @@ function queryTutorials() {
       dataType: 'json'
   })
   .done((json) => {
-    
-    console.log($($('#carouselExampleControls2 .card').get(0)).find('.card-img-top'))
-    for(let i = 0; i < json.length; i++) {
-      $($('#carouselExampleControls2 .card').get(i)).find('._card-img-top')
-        .attr("src", json[i].thumb_url);
-      $($('#carouselExampleControls2 .card').get(i)).find('.card-title')
-        .text(json[i].title);
-      $($('#carouselExampleControls2 .card').get(i)).find('.card-text')
-        .text(json[i]['sub-title']);
-      $($('#carouselExampleControls2 .card').get(i)).find('.creator img')
-        .attr("src", json[i]['author_pic_url']);
-      $($('#carouselExampleControls2 .card').get(i)).find('.creator h6')
-        .text(json[i]['author']);
-      $($('#carouselExampleControls2 .card').get(i)).find('.info span')
-        .text(json[i]['duration']);
-        
-      for(let k = 0; k < json[i].star; k++) {
-        const startImg = $(`
-          <img
-                              src="images/star_on.png"
-                              alt="star on"
-                              width="15px"
-                            />
-          `)
-        $($('#carouselExampleControls2 .card').get(i)).find('.rating').append(startImg)
-      }
-     }
-     displayLoading(false);
+    const html = getPopularHtml(json);
+    const carouselInner = $('#carouselExampleControls2 .carousel-inner');
+    carouselInner.append(html);
+    displayLoading(false, "#carouselExampleControls2");
   })
   .fail((xhr, status, errorThrown) => {
       console.log(`Error: ${errorThrown}`);
@@ -159,9 +132,104 @@ function queryTutorials() {
   });
 }
 
+if(window.matchMedia("(min-width: 576px)").matches) {
+  const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+        interval: false
+    });
+  let scrollPosition = 0;
+  const nextBtn = document.querySelector('#carouselExampleControls2 .carousel-control-next.arrow-right');
+
+  nextBtn.addEventListener('click', () => {
+    let carouselWidth = $("#carouselExampleControls2 .carousel-inner")[0].scrollWidth;
+    let cardWidth = $("#carouselExampleControls2 .carousel-item").width();
+        if(scrollPosition < (carouselWidth - (cardWidth * 4))) {
+        scrollPosition = scrollPosition + cardWidth;
+        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
+        } else {
+          scrollPosition = scrollPosition + cardWidth;
+        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
+        }
+    });
+    $('#carouselExampleControls2 .carousel-control-prev.arrow-left').on('click', function() {
+      let cardWidth = $("#carouselExampleControls2 .carousel-item").width();
+        if(scrollPosition > 0) {
+        scrollPosition = scrollPosition - cardWidth;
+        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
+        } else {
+          scrollPosition = scrollPosition - cardWidth;
+        $("#carouselExampleControls2 .carousel-inner").animate({scrollLeft: scrollPosition}, 600);
+        }
+    });
+
+}else if (window.matchMedia("(max-width: 576px)").matches){
+   // $(multipleItemCarousel).addClass('slide');
+}
+
+  // get Popular Tutorials HTML    
+    function getPopularHtml(data) {
+      let html = '';
+      $.each(data, (index, object) => {
+        let ratingStar = '';
+        for(let i = 0; i < object.star; i++) {
+          ratingStar += `
+            <img
+              src="images/star_on.png"
+              alt="star on"
+              width="15px"
+            />
+          `
+        }
+        html += `
+          <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                <div class="card card-1">
+                  <div class="img-wrapper">
+                    <img
+                      src="${object['thumb_url']}"
+                      class="_card-img-top"
+                      alt="Video thumbnail"
+                    />
+                  </div>
+                  <div class="card-img-overlay text-center">
+                    <img
+                      src="images/play.png"
+                      alt="Play"
+                      width="64px"
+                      class="align-self-center play-overlay"
+                    />
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title font-weight-bold">${object.title}</h5>
+                    <p class="card-text text-muted">
+                      ${object['sub-title']}
+                    </p>
+                    <div class="creator d-flex align-items-center">
+                      <img
+                        src="${object['author_pic_url']}"
+                        alt="Creator of
+                            Video"
+                        width="30px"
+                        class="rounded-circle"
+                      />
+                      <h6 class="pl-3 m-0 main-color">${object.author}</h6>
+                    </div>
+                    <div class="info pt-3 d-flex justify-content-between">
+                      <div class="rating">
+                        ${ratingStar}
+                      </div>
+                      <span class="main-color">${object['duration']}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          `
+      });
+      return html;
+    }
+    //
+
 // Query videos
 function queryVideos() {
-  displayLoading(true);
+  //displayLoading(true);
   const dataObj = {
       action: 'query',
       format: 'json',
@@ -201,7 +269,7 @@ function queryVideos() {
         $($('#carouselExampleControls3 .card').get(i)).find('.rating').append(startImg)
       }
      }
-     displayLoading(false);
+     //displayLoading(false);
   })
   .fail((xhr, status, errorThrown) => {
       console.log(`Error: ${errorThrown}`);
